781078e8740e54396dd542a4577100779fb09a93
hevert83 <101134285+hevert83@users.noreply.github.com>
2023-05-26 14:34:50 -0600

    Add files via upload

---
+++ voletos/firebase.js
@@ -0,0 +1,10 @@
+  var config = {
+    apiKey: "AIzaSyDLVUII-roKCzEa4Zc4QkbkHn72dA57Syk",
+    authDomain: "sistemasdistribuidos-fd172.firebaseapp.com",
+    databaseURL: "https://sistemasdistribuidos-fd172-default-rtdb.firebaseio.com",
+    projectId: "sistemasdistribuidos-fd172",
+    storageBucket: "sistemasdistribuidos-fd172.appspot.com",
+    messagingSenderId: "985784991847",
+    appId: "1:985784991847:web:0f7cf8daf398a5448054c2"
+  };
+  firebase.initializeApp(config);
